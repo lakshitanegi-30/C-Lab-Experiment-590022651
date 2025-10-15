7cdcc36fe3f4bd4ae0d47f92572ba38520f0b5a0
lakshitanegi-30 <lakshitanegi3012@gmail.com>
2025-10-15 16:42:50 +0530

    Create ques2.c

---
+++ Experiment 01/ques2.c
@@ -0,0 +1,12 @@
+// Write a C Program to print the address in multiple lines (new line)
+
+ #include <stdio.h> 
+ int main()  
+{ 
+    printf("Lakshita Negi\n”); 
+    printf(“20,Jagriti Vihar\n”); 
+    printf(“Jogiwala\n”); 
+    printf(“Dehradun-248005\n”); 
+ 
+       return 0; 
+ }
