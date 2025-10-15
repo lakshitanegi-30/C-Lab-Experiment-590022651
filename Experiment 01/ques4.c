aa30d5593d25976ef5c0add89de3044e0085dc92
lakshitanegi-30 <lakshitanegi3012@gmail.com>
2025-10-15 16:48:08 +0530

    Create ques4.c

---
+++ Experiment 01/ques4.c
@@ -0,0 +1,15 @@
+// Write a C program to add two numbers, take number from user.
+
+ #include <stdio.h> 
+ int main() 
+{ 
+  int a,b; 
+ 
+  printf("Enter value of a: "); 
+  scanf("%d", &a); 
+  printf("Enter value of b: "); 
+  scanf("%d", &b); 
+  printf("Sum = %d",a+b); 
+ 
+    return 0; 
+}
